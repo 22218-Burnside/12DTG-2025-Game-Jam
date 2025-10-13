14855dc82bdb57aa433013b83c014df2ff270fd1
22236bhs <22236@burnside.school.nz>
2025-10-14 10:27:10 +1300

    made the enemy move towards the player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,24 @@
 extends Area2D
+
+
 var damage = 10
+
+const SPEED : int = 150
+
+
+
+var player : Node2D
+
 signal enemy_hit_player
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+
+func _ready() -> void:
+	player = get_parent().get_node("player")
+
+
+func _physics_process(delta: float) -> void:
+	position += SPEED * position.direction_to(player.position) * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
