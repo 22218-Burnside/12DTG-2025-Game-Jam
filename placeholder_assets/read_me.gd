fcfbb3b37ab5c0b7034ec6e0fb5d36a3699e042f
Flamerider347 <flamerider995@gmail.com>
2025-10-15 13:24:46 +1300

    added readme for placeholder assets

---
+++ placeholder_assets/read_me.gd
@@ -0,0 +1,10 @@
+"""
+# Placeholder Assets
+
+This folder contains temporary images, sounds, and files for game development.  
+These assets are not final and should ONLY be used for testing and prototyping.  
+Replace them with official assets before release.
+
+Do not distribute or use outside of development!
+
+"""
