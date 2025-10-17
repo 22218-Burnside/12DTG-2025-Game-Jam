f27b80ca1357d2a916bb782dee4d1111a4d284da
22236bhs <22236@burnside.school.nz>
2025-10-17 20:31:30 +1300

    added element ui that updates when changed

--- element_slot.gd
+++ element_slot.gd
@@ -1,4 +1,12 @@
+@tool
 extends VBoxContainer
+@onready var button: Button = $Button
+@onready var label: Label = $Label
 @export var slot_id : int = 0 
-@export var element : Element 
+@export var element : Element
 @export var element_level : int = 1
+
+func _process(_delta: float) -> void:
+	if element:
+		button.icon = element.element_texture
+		label.text = str(element.element_name, " LVL", element_level)
--- scripts/element.gd
+++ scripts/element.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name Element
 
 @export var element_texture : Texture2D
+@export var element_name : String
